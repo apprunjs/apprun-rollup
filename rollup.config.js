8ee80e877ef47d22f48f09ec616a469de18056c2
Yiyi Sun <yiyisun@gmail.com>
2019-06-19 11:44:35 -0400

    use rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import { terser } from 'rollup-plugin-terser';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import nodeResolve from "rollup-plugin-node-resolve";
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'bin/index.js',
+  input: 'src/index.tsx',
   output: {
     file: 'public/index.js',
     format: 'esm',
@@ -11,6 +12,7 @@ export default {
   },
   plugins: [
     nodeResolve(),
+    typescript({ tsconfig: "src/tsconfig.json" }),
     terser({warnings: true, module: true, mangle: { properties: { regex: /^__/ }}}),
     sourcemaps(),
   ]
